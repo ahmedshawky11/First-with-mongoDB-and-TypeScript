6c2d567a34cb0626e8aa3bbe8041153147034b3a
Ahmed Shawky <63034550+ahmedshawky11@users.noreply.github.com>
2023-11-07 18:49:54 +0200

    Update database.ts

--- database.ts
+++ database.ts
@@ -17,17 +17,3 @@ async function read(collectionName: string, filter: object = {}) {
   return documents;
 }
 
-import { save, read } from "./database";
-
-// Save a document to the database
-const document = {
-  name: "Ahmed Shawky",
-  age: 24,
-};
-await save("my_collection", document);
-
-// Read all documents from the database
-const documents = await read("my_collection");
-
-// Print the value of the documents variable to the console
-console.log(documents);
